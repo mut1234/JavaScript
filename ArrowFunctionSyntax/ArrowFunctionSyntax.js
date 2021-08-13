ce161099f7183178c1ac8cf461ebb5673960d43f
Mustafa Ghnaim <44782684+mut1234@users.noreply.github.com>
2021-08-13 18:00:16 +0300

    Add files via upload

---
+++ ArrowFunctionSyntax/ArrowFunctionSyntax.js
@@ -0,0 +1,24 @@
+/*
+Function
+--- Arrow Function
+--- Regular vs Arrow
+--- Muliple Lines
+*/
+
+
+/*let print = function print(){
+    return 10;
+};*/
+
+//let print = () => 10; //for one line only
+
+// let print = _ => 10; //for one line only (you can use _ insted of () )
+
+// let print = num => num;
+
+/* let print = function (num1,num2){
+    return num1+num2;
+} */
+
+let print = (num1,num2) => num1 + num2 ;
+console.log(print(50,60));
